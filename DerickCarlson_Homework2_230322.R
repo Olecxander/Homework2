ecc5922018aa1b78c26e791cab927295c4cd89e6
Olecxander <derickcarlson.git@gmail.com>
2023-03-22 14:13:34 -0500

    Initial commi
    Initial commit

---
+++ DerickCarlson_Homework2_230322.R
@@ -0,0 +1,5 @@
+Norm_Rainfall.in.mm <- read.csv("~/MICA/Stats1/Homework2/Norm_Rainfall_in_mm.csv")
+View(Norm_Rainfall.in.mm)
+
+Table_Rainfall.in.mm <- read.csv("~/MICA/Stats1/Homework2/Table_Rainfall_in_mm.csv")
+View(Norm_Rainfall.in.mm)
