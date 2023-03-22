7844fbc273220d6fbfff863bcd4d34e76e21493f
Olecxander <derickcarlson.git@gmail.com>
2023-03-22 14:55:23 -0500

    Homework Complete

--- DerickCarlson_Homework2_230322.R
+++ DerickCarlson_Homework2_230322.R
@@ -1,5 +1,18 @@
-Norm_Rainfall.in.mm <- read.csv("~/MICA/Stats1/Homework2/Norm_Rainfall_in_mm.csv")
-View(Norm_Rainfall.in.mm)
+norm_df <- read.csv("~/MICA/Stats1/Homework2/Norm_Rainfall_in_mm.csv"
+                    , header = TRUE)
+#View(Norm_Rainfall.in.mm)
 
-Table_Rainfall.in.mm <- read.csv("~/MICA/Stats1/Homework2/Table_Rainfall_in_mm.csv")
-View(Norm_Rainfall.in.mm)
+table_df <- read.csv("~/MICA/Stats1/Homework2/Table_Rainfall_in_mm.csv"
+                     , header = TRUE)
+#View(Norm_Rainfall.in.mm)
+
+simiyu_df <- norm_df[norm_df$Lake == "Simiyu",]
+norm_mean <- mean(simiyu_df$Rainfall_in_mm)
+norm_median <- median(simiyu_df$Rainfall_in_mm)
+table_mean <- mean(table_df$Simiyu)
+table_median <- median(table_df$Simiyu)
+
+print(norm_mean)
+print(norm_median)
+print(table_mean)
+print(table_median)
